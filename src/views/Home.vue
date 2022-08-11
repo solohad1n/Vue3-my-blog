dbf94802f01913f5b54a569d5f13efd35b9407f3
solohad1n <fox663399@gmail.com>
2022-08-11 20:21:12 +0600

    receiving data from the backend and rendering this data in posts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,9 +10,24 @@
 <script>
 import PostsList from "@/components/PostsList.vue";
 import { ref } from "@vue/reactivity";
+import { onMounted } from "@vue/runtime-core";
 export default {
   setup() {
     const posts = ref([]);
+    const error = ref(null);
+
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/posts");
+        posts.value = await response.json();
+      } catch (err) {
+        error.value = err.message;
+      }
+    };
+
+    onMounted(() => {
+      fetchPosts();
+    });
 
     return { posts };
   },
